8068018d2e001a59942d490b2a2acf276c5cd290
marcosmvr <marcosviniciusreis260@gmail.com>
2025-03-20 17:04:47 -0300

    rotas configuradas

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-import express from 'express'
+const express = require('express')
+const router = require('./routers/index')
 const app = express()
 const port = 3000
 
-app.listen(3000, error => {
+router(app)
+
+app.listen(port, error => {
   if (error) {
     console.log('Deu erro')
     return
